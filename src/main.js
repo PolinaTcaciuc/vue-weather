cafc4c133a2c7c76f7d970314c29f2ed9a3f8b94
Polina Tcaciuc <polinatcaciuc@gmail.com>
2023-03-08 16:56:37 +0300

    init: start vue-weather project

--- src/main.js
+++ src/main.js
@@ -3,5 +3,12 @@ import App from './App.vue'
 import './registerServiceWorker'
 import router from './router'
 import store from './store'
+/* -------- styles --------  */
+import '@/assets/styles/index.scss'
 
-createApp(App).use(store).use(router).mount('#app')
+
+const app = createApp(App);
+app
+.use(store)
+.use(router)
+.mount('#app')
